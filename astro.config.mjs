5dde87ec42c1b83ef6f64a393b8c463170a68e2b
Redzhep Molaahmed <redzhep@analytics.co.uk>
2024-10-02 21:04:57 +0300

    feat: Add @astrojs/cloudflare dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   integrations: [tailwind(), react()],
   markdownOptions: {
     syntaxHighlight: 'shiki',
     // or 'prism' for different highlighting options
     remarkPlugins: [],
     rehypePlugins: []
-  }
+  },
+  adapter: cloudflare()
 });
