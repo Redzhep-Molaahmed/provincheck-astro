565978652aba9a15e2c88a8b06833b707a885441
Redzhep Molaahmed <redzhep@analytics.co.uk>
2024-10-06 14:24:39 +0300

    ```text feat: Update landing page to display country code
    The landing page has been updated to display the country code instead of showing different welcome messages based on the country. This change improves the user experience by providing more relevant information.

--- src/utils/geo.ts
+++ src/utils/geo.ts
@@ -1,5 +1,10 @@
 export async function getLocation(ip) {
     const response = await fetch(`http://ip-api.com/json/${ip}`);
     const data = await response.json();
-    return data.countryCode;
+    return {
+      city: data.city,
+      country: data.country,
+      lat: data.lat,
+      lon: data.lon,
+    };
   }
